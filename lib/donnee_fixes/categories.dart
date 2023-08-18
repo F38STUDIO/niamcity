af0233c914b66e7f01476a12916419ad350e2dd6
LeGrandF38 <faycalyacoubaprog@gmail.com>
2023-08-18 14:04:47 +0100

    mise à jour des données fixe <<categories>> (liste terminée)

--- lib/donnee_fixes/categories.dart
+++ lib/donnee_fixes/categories.dart
@@ -1,9 +1,53 @@
+import 'package:flutter/material.dart';
 import 'package:niamcity/models/categorie.dart';
 
 List<Categorie> types = [
   Categorie(
     id: "001",
-    nom: "clinique",
+    nom: "Santé",
+    icon: Icons.favorite,
     img: "assets/001.jpeg",
+  ),
+  Categorie(
+    id: "002",
+    nom: "Education",
+    icon: Icons.school,
+    img: "",
+  ),
+  Categorie(
+    id: "003",
+    nom: "logement",
+    icon: Icons.home,
+    img: "",
+  ),
+  Categorie(
+    id: "004",
+    nom: "Transport",
+    icon: Icons.directions_bus,
+    img: "",
+  ),
+  Categorie(
+    id: "005",
+    nom: "commerce",
+    icon: Icons.shopping_cart,
+    img: "",
+  ),
+  Categorie(
+    id: "006",
+    nom: "Tourisme",
+    icon: Icons.airplanemode_active,
+    img: "",
+  ),
+  Categorie(
+    id: "007",
+    nom: "Loisirs",
+    icon: Icons.sports_soccer,
+    img: "",
+  ),
+  Categorie(
+    id: "008",
+    nom: "Divers",
+    icon: Icons.more_horiz,
+    img: "",
   ),
 ];
--- lib/models/categorie.dart
+++ lib/models/categorie.dart
@@ -1,10 +1,15 @@
+import 'package:flutter/material.dart';
+
 class Categorie {
   String id;
   String nom;
+  IconData icon;
   String img;
+
   Categorie({
     required this.id,
     required this.nom,
+    required this.icon,
     required this.img,
   });
 }
