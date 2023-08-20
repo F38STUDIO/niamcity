98e305067febc97fc42507451ad51fa59c353fc0
LeGrandF38 <faycalyacoubaprog@gmail.com>
2023-08-20 14:34:03 +0100

    BottomnaviagationBar terminée

--- lib/donnee_fixes/couleurs.dart
+++ lib/donnee_fixes/couleurs.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 // Cette classe contient toutes les couleurs utilisés dans l'application
 class Couleurs {
-  final Color a = const Color(0xFFFFFFFF);
-  final Color b = const Color(0xFF5FC2BA);
-  final Color c = const Color(0xFF3B556D);
-  final Color d = const Color(0xFF1C2942);
-  final Color e = const Color(0xFF0B162C);
+  static const Color a = Color(0xFFFFFFFF);
+  static const Color b = Color(0xFF5FC2BA);
+  static const Color c = Color(0xFF3B556D);
+  static const Color d = Color(0xFF1C2942);
+  static const Color e = Color(0xFF0B162C);
 }
 // Couleurs d'origine
 //#FFFFFF
---
+++ lib/screens/main_page.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+
+import '../donnee_fixes/couleurs.dart';
+import 'home_page.dart';
+import 'location_page.dart';
+import 'profile_page.dart';
+import 'search_page.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ProvidedStylesExample(
+      menuScreenContext: context,
+    );
+  }
+}
+
+// ----------------------------------------- Provided Style ----------------------------------------- //
+
+class ProvidedStylesExample extends StatefulWidget {
+  const ProvidedStylesExample({final Key? key, required this.menuScreenContext})
+      : super(key: key);
+  final BuildContext menuScreenContext;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ProvidedStylesExampleState createState() => _ProvidedStylesExampleState();
+}
+
+class _ProvidedStylesExampleState extends State<ProvidedStylesExample> {
+  late PersistentTabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController();
+  }
+
+  List<Widget> _buildScreens() => [
+        const HomePage(),
+        const LocationPage(),
+        const SearchPage(),
+        const ProfilePage(),
+      ];
+
+  List<PersistentBottomNavBarItem> _navBarsItems() => [
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          title: "Acceuil",
+          activeColorPrimary: Couleurs.b,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.location_on),
+          title: "Localisation",
+          activeColorPrimary: Couleurs.b,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.search),
+          title: "Recherche",
+          activeColorPrimary: Couleurs.b,
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.person),
+          title: "Profil",
+          activeColorPrimary: Couleurs.b,
+          inactiveColorPrimary: Colors.grey,
+        ),
+      ];
+
+  @override
+  Widget build(final BuildContext context) => Scaffold(
+        body: PersistentTabView(
+          context,
+          controller: _controller,
+          screens: _buildScreens(),
+          items: _navBarsItems(),
+          resizeToAvoidBottomInset: true,
+          navBarHeight: MediaQuery.of(context).viewInsets.bottom > 0
+              ? 0.0
+              : kBottomNavigationBarHeight,
+          bottomScreenMargin: 0,
+          // onWillPop: (final context) async {
+          //   await showDialog(
+          //     context: context,
+          //     useSafeArea: true,
+          //     builder: (final context) => Container(
+          //       height: 50,
+          //       width: 50,
+          //       color: Colors.white,
+          //       child: ElevatedButton(
+          //         child: const Text("Close"),
+          //         onPressed: () {
+          //           Navigator.pop(context);
+          //         },
+          //       ),
+          //     ),
+          //   );
+          //   return false;
+          // },
+
+          backgroundColor: Couleurs.d,
+          hideNavigationBar: false,
+          decoration: const NavBarDecoration(colorBehindNavBar: Colors.indigo),
+          itemAnimationProperties: const ItemAnimationProperties(
+            duration: Duration(milliseconds: 400),
+            curve: Curves.ease,
+          ),
+          screenTransitionAnimation: const ScreenTransitionAnimation(
+            animateTabTransition: true,
+          ),
+          navBarStyle: NavBarStyle
+              // .style19,
+              .style19, // Choose the nav bar style with this property
+        ),
+      );
+}
