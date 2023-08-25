f9ba724a6839ce28ead0ffa7c038502b53ab1da5
Issaka <moussasidikouissaka019@gmail.com>
2023-08-25 16:51:56 +0100

    modification home page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:niamcity/donnee_fixes/couleurs.dart';
 
 import '../donnee_fixes/constantes.dart';
 
@@ -10,32 +11,53 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text("NiamCity"),
+        title: const Text(
+          "NiamCity",
+          style: TextStyle(
+              color: Couleurs.e, fontSize: 30.0, fontWeight: FontWeight.w900),
+        ),
       ),
-      body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 12.0,
-            crossAxisSpacing: 12.0,
-          ),
-          itemCount: 8,
-          itemBuilder: (BuildContext context, int index) {
-            return Card(
-              elevation: 4.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    listCategorie[index]['icon'],
-                    size: 48.0,
-                    color: Colors.blue,
-                  ),
-                  const SizedBox(height: 8.0),
-                  Text(listCategorie[index]['name']),
-                ],
+      body: CustomScrollView(
+        slivers: [
+          SliverPadding(
+            padding: const EdgeInsets.all(12.0),
+            sliver: SliverGrid(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 12.0,
+                crossAxisSpacing: 12.0,
               ),
-            );
-          }),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  return Card(
+                    color: Couleurs.d,
+                    elevation: 4.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          listCategorie[index]['icon'],
+                          size: 48.0,
+                          color: Couleurs.b,
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          listCategorie[index]['name'],
+                          style: const TextStyle(
+                              color: Couleurs.a,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                childCount: 8,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
