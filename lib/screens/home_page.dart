ec33176088590f38e3835b9bab2df746af78ec02
Issaka <moussasidikouissaka019@gmail.com>
2023-08-25 14:19:42 +0100

    ajout du fichier contante et modification de home_page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
+import '../donnee_fixes/constantes.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Home Page"),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("NiamCity"),
       ),
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 12.0,
+            crossAxisSpacing: 12.0,
+          ),
+          itemCount: 8,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              elevation: 4.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    listCategorie[index]['icon'],
+                    size: 48.0,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(height: 8.0),
+                  Text(listCategorie[index]['name']),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
