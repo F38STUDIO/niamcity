d9aa428e611b76d5523a3aec0a0ffc8043982b29
Issaka <moussasidikouissaka019@gmail.com>
2023-09-19 15:08:48 +0100

    login registraton profil 1 & 2

--- lib/models/utilisateur.dart
+++ lib/models/utilisateur.dart
@@ -1,10 +1,12 @@
 class Utilisateur {
   String id;
   String nom;
+  String address;
   int numero;
   Utilisateur({
     required this.id,
     required this.nom,
+    required this.address,
     required this.numero,
   });
 }
